f425f7b682ba777f8dc063ae4cba502671b49843
yegaoteng <2287681747@qq.com>
2026-05-19 22:17:56 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,7 +33,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.micostar.cc",
+    site: "https://fuwari.ffurry.cc.cd",
     base: "/",
     trailingSlash: "always",
     integrations: [tailwind({
